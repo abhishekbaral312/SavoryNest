b8c27ce9a9fee3ce931f1aa406e15f3d51d344a5
unknown <abhishekbaral312@gmail.com>
2025-08-28 19:12:45 +0530

    Add Menu categories

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -8,11 +8,72 @@ function Menu() {
   return (
     <>
     <SubBanner name="Our Menu" page="Menu"/>
+    <section className="menu-heading">
+      <div className="container">
+        <div className="text-center">
+            <p className="section-title mb-2">● Taste The Best</p>
+            <h1 className="sec-head">OUR <span> EXCLUSIVE</span> MENU</h1>
+            <p>Indulge in handcrafted dishes made with passion and premium ingredients. From bold starters to refreshing beverages, our menu is curated to offer flavors you won’t forget. Each plate may surprise your palate and elevate your dining journey.</p>
+          </div>
+          <div className="menu-cat">
+            <div className="row">
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("starters")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-1.png" alt="category" />
+                  <h3 className="mt-4">Starters</h3>
+                </div>
+              </div>
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("veg-platter")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-2.png" alt="category" />
+                  <h3 className="mt-4">Veg Platter</h3>
+                </div>
+              </div>
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("seafoods")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-3.png" alt="category" />
+                  <h3 className="mt-4">Sea Foods</h3>
+                </div>
+              </div>
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("desserts")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-4.png" alt="" srcset="" />
+                  <h3 className="mt-4">Desserts</h3>
+                </div>
+              </div>
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("beverages")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-5.png" alt="" srcset="" />
+                  <h3 className="mt-4">Bevarages</h3>
+                </div>
+              </div>
+              <div className="col-md-2 col-6">
+                <div className="text-center" onClick={() =>
+      document.getElementById("salads-&-soups")?.scrollIntoView({ behavior: "smooth" })
+    }>
+                  <img src="./images/cat-6.png" alt="" srcset="" />
+                  <h3 className="mt-4">Salads & Soups</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
     <section className="menu-body">
         {categories.map((cat,index) => (
           <div className="menu-type">
           <div className="container">
-        <div className="row" key={index}>
+        <div className="row" key={index} id={cat.replace(/\s+/g, "-").toLowerCase()}>
           <div className="col-md-2">
               <p className="section-title mb-2">● Menu & Pricing</p>
               <h2 className="menu-head">{cat}</h2>
