7947a4533779c90a68bff09dd5b855709303d8a9
unknown <abhishekbaral312@gmail.com>
2025-09-01 14:43:19 +0530

    preloader add

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { lazy, useEffect, useState } from "react";
 import ScrollToTop from "./scrollToTop";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -14,16 +15,39 @@ const Checkout = lazy(() => import("./pages/Checkout"));
 const ThankYou = lazy(() => import("./pages/Thankyou"));
 const Contact = lazy(() => import("./pages/Contact"));
 
+function PageWrapper({ children }) {
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const minTime = 2000; 
+    setLoading(true);
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, minTime);
+
+    return () => clearTimeout(timer);
+  }, [location]); 
+
+  return <>{loading ? <Preloader /> : children}</>;
+}
+
 function App() {
+
   useEffect(() => {
     AOS.init({
-      duration: 1000, // animation duration in ms
-      once: false, // run only once
+      duration: 1000, 
+      once: false, 
     });
   }, []);
+
+
   return (
+    
     <Router>
       <ScrollToTop/>
+      <PageWrapper>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -35,7 +59,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
+      </PageWrapper>
     </Router>
+    
   );
 }
 
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,10 @@
+
+function Preloader() {
+  return (
+   <div className="loader">
+    <img src="./images/loader.gif" alt="Loading..."/>
+   </div>
+  );
+}
+
+export default Preloader;
